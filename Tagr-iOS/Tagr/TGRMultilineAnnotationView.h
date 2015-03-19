033dfb18f44510b6bc8ea71c462f82ba668b5ca3
David Bereza <velonicv@gmail.com>
2015-03-19 16:44:48 -0700

    report button cleanup

---
+++ Tagr-iOS/Tagr/TGRMultilineAnnotationView.h
@@ -0,0 +1,13 @@
+//
+//  TGRMultilineAnnotationView.h
+//  Tagr
+//
+//  Created by Davs on 3/18/15.
+//
+//
+
+#ifndef Tagr_TGRMultilineAnnotationView_h
+#define Tagr_TGRMultilineAnnotationView_h
+
+
+#endif
---
+++ Tagr-iOS/Tagr/TGRMultilineAnnotationView.m
@@ -0,0 +1,9 @@
+//
+//  TGRMultilineAnnotationView.m
+//  Tagr
+//
+//  Created by Davs on 3/18/15.
+//
+//
+
+#import <Foundation/Foundation.h>
